2849fdb9dcf31dff597e3fb0823b09b592dfbffe
rmust <ramunas.musteik@gmail.com>
2022-12-22 01:51:34 +0200

    up

--- src/MUIWrapper.tsx
+++ src/MUIWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import { Box, createTheme, Paper, ThemeProvider } from "@mui/material";
 
 const darkTheme = createTheme({
--- src/test/utils.ts
+++ src/test/utils.ts
@@ -6,11 +6,6 @@ import { Endpoint } from "../services/types";
 export const renderWithRouter = (element: ReactElement) =>
   render(element, { wrapper: BrowserRouter });
 
-const okResponseBase = {
-  ok: true,
-  status: 200,
-};
-
 export const mockLoginFailure = async (
   input: RequestInfo | URL,
   init?: RequestInit
